77a044fb50a87f7f2e82aa0b6944efd835edda05
Ron Nofar <ronnofar2@gmail.com>
2018-02-09 12:27:51 -0800

    Auto Push
    Tag 0.0.1.3

---
+++ pypiUploader.py
@@ -0,0 +1,23 @@
+from subprocess import check_output
+
+with open('setup.py', 'r') as file:
+    data = file.readlines()
+print("Current",data[0])
+
+print("Tag:")
+tag = input(">>> ")
+
+data[0] = "tag = \'%s\'\n"%tag
+with open('setup.py', 'w') as file:
+    file.writelines(data)
+
+# stage all files
+check_output("git add *", shell=True).decode()
+check_output(
+    "git commit -a -m \"Auto Push\" -m \"Tag %s\""%tag,
+    shell=True).decode()
+check_output("git tag %s"%tag, shell=True).decode()
+check_output("git push --tags origin master", shell=True).decode()
+check_output("python setup.py sdist upload -r pypi", shell=True).decode()
+
+
