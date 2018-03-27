3fe739f7d7d02fe9e02fcb378122cde20190ef49
Ron Nofar <ronnofar2@gmail.com>
2018-03-27 12:06:17 -0700

    Auto Push
    Tag 0.0.2.1.7

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-tag = '0.0.2.1.6'
+tag = '0.0.2.1.7'
 from setuptools import setup, find_packages
 #from distutils.core import setup
 setup(
@@ -12,6 +12,7 @@ setup(
   download_url = 'https://github.com/RonNofar/RToolPackage/archive/%s.tar.gz'%tag, # I'll explain this in a second
   keywords = ['RTool', 'win32', 'Maya'], # arbitrary keywords
   classifiers = [],
+  package_data={'', ['RTool/ffmpeg/bin/ffmpeg.exe']}
   include_package_data=True
 )
 
