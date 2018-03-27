7c214f444d11bc3ceb6b6fb4ed34473d7da2d3ce
Ron Nofar <ronnofar2@gmail.com>
2018-03-27 11:47:56 -0700

    Auto Push
    Tag 0.0.2.1.5

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-tag = '0.0.2.1.4'
+tag = '0.0.2.1.5'
 from setuptools import setup, find_packages
 #from distutils.core import setup
 setup(
@@ -12,6 +12,7 @@ setup(
   download_url = 'https://github.com/RonNofar/RToolPackage/archive/%s.tar.gz'%tag, # I'll explain this in a second
   keywords = ['RTool', 'win32', 'Maya'], # arbitrary keywords
   classifiers = [],
+  include_package_data=True
 )
 
 #http://wiki.python.org/moin/Distutils/Cookbook/AutoPackageDiscovery
